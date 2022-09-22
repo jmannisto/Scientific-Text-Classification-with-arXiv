b94f2893a808311e816114862a454f88c12d97fe
jmannisto <67613785+jmannisto@users.noreply.github.com>
2022-09-22 23:17:19 +0300

    Create dataVisualization.py

---
+++ dataVisualization.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from wordcloud import WordCloud
+
+
+#TODO: visualization of test/train data by category
+
+#TODO: wordcloud of high use words across categories
+
+#TODO: wordcloud of high use words within categories
+
+#TODO: plot the tdif scores of words?
