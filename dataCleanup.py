bcea3d37892ddb3ad397aab8954537944db193a5
VilhelmHovland <114264416+VilhelmHovland@users.noreply.github.com>
2022-09-27 14:26:45 +0000

    Update dataCleanup.py
    Added tokeization and updated other data cleanup correspondingly

--- dataCleanup.py
+++ dataCleanup.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import json
 from nltk.corpus import stopwords #note: make sure to have nltk downloaded
+from nltk.tokenize import word_tokenize
 
 #if we want to use LabelEncoder for converting our categories
 #from sklearn.preprocessing import LabelEncoder 
@@ -9,19 +10,24 @@ from nltk.corpus import stopwords #note: make sure to have nltk downloaded
 data = pd.read_json('arxivSmall.json')
 #NOTE: we can also use arxiv API by using arxiv on github: https://github.com/lukasschwab/arxiv.py
 
+#Tokenize
+data['abstract'] = word_tokenize(data['abstract'])
+
 #lowercase
-data['abstract'] = data['abstract'].str.lower()
+data['abstract'] = [token.str.lower() for token in data['abstract']]
 
 #TODO: Convert categories
 # data['abstract'] = data['abstract'].replace([what], [with what?])
 #can also use LabelEncoder
 
 #remove numbers and punctuation
-data['abstract'] = data['abstract'].str.replace(r'[^\w\s]+', '', regex=True)
+data['abstract'] = [token for token in data['abstract'] if token != r'[\W(\d.*)]']
+#removes any token that is non-alphanumeric or begins with a number
 
 #TODO: remove stop words 
 #QUESTION: should this be done before or after the punctuation? 
 stop = stopwords.words('english')
-test['abstract'] = test['abstract'].apply(lambda x: ' '.join([word for word in x.split() if word not in (stop)]))
+data['abstract'] = [token for token in data['abstract'] if token not in stop]
+#test['abstract'] = test['abstract'].apply(lambda x: ' '.join([word for word in x.split() if word not in (stop)]))
 
 #TODO: split into test and train data
