fcd8adacd1ebb2e9835b24d87495c0d24fbde6d6
VilhelmHovland <114264416+VilhelmHovland@users.noreply.github.com>
2022-09-29 08:17:20 +0000

    Create dataCleanup.py
    Fixed text normalization to work with DataFrame

--- dataCleanup.py
+++ dataCleanup.py
@@ -2,7 +2,6 @@ import pandas as pd
 import json
 from nltk.corpus import stopwords #note: make sure to have nltk downloaded
 from nltk.tokenize import word_tokenize
-from sklearn.feature_extraction.text import TfidfVectorizer
 
 #if we want to use LabelEncoder for converting our categories
 #from sklearn.preprocessing import LabelEncoder 
@@ -12,29 +11,20 @@ data = pd.read_json('arxivSmall.json')
 #NOTE: we can also use arxiv API by using arxiv on github: https://github.com/lukasschwab/arxiv.py
 
 #Tokenize
-data['abstract'] = word_tokenize(data['abstract'])
+data['abstract'].apply(lambda x: word_tokenize(x), axis=1)
 #data['abstract'] = data['abstract'].apply(word_tokenize)
 
 #lowercase
-data['abstract'] = [token.str.lower() for token in data['abstract']]
+data['abstract'].apply(lambda x: [token.str.lower() for token in x], axis=1)
 
 #remove numbers and punctuation
-data['abstract'] = [token for token in data['abstract'] if token != r'[\W(\d.*)]']
+data['abstract'].apply(lambda x: [token for token in x if token != r'[\W(\d.*)]'], axis=1)
 #removes any token that is non-alphanumeric or begins with a number
 
 #remove stop words  
 stop = stopwords.words('english')
-data['abstract'] = [token for token in data['abstract'] if token not in stop]
+data['abstract'].apply(lambda x: [token for token in x if token not in stop], axis=1)
 #test['abstract'] = test['abstract'].apply(lambda x: ' '.join([word for word in x.split() if word not in (stop)]))
-
-#TODO: tfidf
-#defaults at first
-#tweak pending results 
-vectorizer = TfidfVectorizer()
-X = vectorizer.fit_transform(data['abstract'])
-tfidf_matrix = vectorizer.get_feature_names_out()
-
-#TODO: normalize # of vectors within each row
 
 #remove category subidentifies (indicated by a period after the category)
 data["categories"] = data["categories"].str.replace(r"\..+\b","", regex=True)
@@ -43,7 +33,7 @@ data["categories"] = data["categories"].str.replace(r'\b(\w+)( \1\b)+', r'\1', r
 
 #TODO: Convert categories
 #convert categories into arrays
-data["categories"] = data["categories"].str.split(' ') #I don't think this actually returns an array though? 
+#
 #hold off on labeling numerically, change if needed during training process
 #data['categories'] = data['categories'].replace([what], [with what?])
 #can also use LabelEncoder
