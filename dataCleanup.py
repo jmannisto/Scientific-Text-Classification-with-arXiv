8bd66c007ad0e0064b7759e714822539dcde8a8a
jmannisto <67613785+jmannisto@users.noreply.github.com>
2022-10-02 16:56:37 +0300

    Update dataCleanup.py
    update tfidf
    rearranged when we tokenize
    updated regex for category cleanup

--- dataCleanup.py
+++ dataCleanup.py
@@ -1,8 +1,8 @@
 import pandas as pd
 import json
 from nltk.corpus import stopwords #note: make sure to have nltk downloaded
-from nltk.tokenize import word_tokenize
-
+from nltk.tokenize import word_tokenize, sent_tokenize
+from sklearn.feature_extraction.text import TfidfVectorizer 
 #if we want to use LabelEncoder for converting our categories
 #from sklearn.preprocessing import LabelEncoder 
 
@@ -10,33 +10,44 @@ from nltk.tokenize import word_tokenize
 data = pd.read_json('arxivSmall.json')
 #NOTE: we can also use arxiv API by using arxiv on github: https://github.com/lukasschwab/arxiv.py
 
-#Tokenize
-data['abstract'].apply(word_tokenize, axis=1)
-#data['abstract'] = data['abstract'].apply(word_tokenize)
+#lowercase - easier to do before tokenizing
+data['abstract'] = data['abstract'].str.lower()
 
-#lowercase
-data['abstract'].apply(lambda x: [token.lower() for token in x], axis=1)
-
-#remove numbers and punctuation
-data['abstract'].apply(lambda x: [token for token in x if token != r'[\W(\d.*)]'], axis=1)
+#remove numbers and punctuation - easier to do before tokenizing 
+data['abstract'] = data['abstract'].str.replace(r'[^\w\s]+', '', regex=True)
 #removes any token that is non-alphanumeric or begins with a number
+
+#do we need to sentence tokenize first?
+#data['abstract'] = data['abstract'].apply(sent_tokenize)
+
+#Tokenize
+data['abstract'] = data['abstract'].apply(word_tokenize)
 
 #remove stop words  
 stop = stopwords.words('english')
-data['abstract'].apply(lambda x: [token for token in x if token not in stop], axis=1)
-#test['abstract'] = test['abstract'].apply(lambda x: ' '.join([word for word in x.split() if word not in (stop)]))
+data['abstract'] = data['abstract'].apply(lambda x: [token for token in x if token not in stop])
 
 #remove category subidentifies (indicated by a period after the category)
-data["categories"] = data["categories"].str.replace(r"\..+\b","", regex=True)
+#remove any instance of '.' followed by at least two letters or . followed by at least two letters, a dash '-' and more letters
+data["categories"] = data["categories"].str.replace(r"\.[A-Za-z]{2,}\b|\.[A-Za-z]{2,}-[A-Za-z]{1,}\b","", regex=True) 
+
 #remove duplicate categories
-data["categories"] = data["categories"].str.replace(r'\b(\w+)( \1\b)+', r'\1', regex=True)
+data["categories"] = data["categories"].str.replace(r'\b([\w-]+)( \1\b)+', r'\1', regex=True)
 
 #TODO: Convert categories
 #convert categories into arrays
-#
+data["categories"] = data["categories"].str.split(' ') 
 #hold off on labeling numerically, change if needed during training process
-#data['categories'] = data['categories'].replace([what], [with what?])
-#can also use LabelEncoder
+
+#TODO: tfidf
+#defaults at first
+#tweak pending results 
+vectorizer = TfidfVectorizer(max_df = 1.0)
+abstracts = data.abstract.astype(str) #can't pass tokens through vectorizer 
+tfidf_matrix = vectorizer.fit_transform(abstracts)
+features = vectorizer.get_feature_names_out()
+
+#TODO: normalize # of vectors within each row
 
 #TODO: split into test and train data
 
