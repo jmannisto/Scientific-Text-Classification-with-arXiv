604d99450c8c5330bc1e6ee03ea0ef20f9ffc85a
jmannisto <67613785+jmannisto@users.noreply.github.com>
2022-10-06 11:11:43 +0300

    Edited dataCleanUp to match notebook

--- dataCleanup.py
+++ dataCleanup.py
@@ -21,18 +21,17 @@ data['abstract'] = data['abstract'].str.lower()
 data['abstract'] = data['abstract'].str.replace(r'[^\w\s]+', '', regex=True)
 #removes any token that is non-alphanumeric or begins with a number
 
-#Tokenize
-data['abstract'] = data['abstract'].apply(word_tokenize)
+#Tokenize - removing for now 
+#data['abstract'] = data['abstract'].apply(word_tokenize)
 
 #remove stop words  
 stop = stopwords.words('english')
-data['abstract'] = data['abstract'].apply(lambda x: [token for token in x if token not in stop])
+data["abstract"] = data['abstract'].apply(lambda x: ' '.join([word for word in str(x).split() if word not in (stop)]))
 
 #lemmatize (set up as if it's not tokenized)
 from nltk.stem import WordNetLemmatizer
 wnl = WordNetLemmatizer()
 data["lemma abstract"] = data["abstract"].apply(lambda x: ' '.join([wnl.lemmatize(word) for word in str(x).split()]))
-data.head()
 
 #remove category subidentifies (indicated by a period after the category)
 #remove any instance of '.' followed by at least two letters or . followed by at least two letters, a dash '-' and more letters
@@ -41,27 +40,27 @@ data["categories"] = data["categories"].str.replace(r"\.[A-Za-z]{2,}\b|\.[A-Za-z
 #remove duplicate categories
 data["categories"] = data["categories"].str.replace(r'\b([\w-]+)( \1\b)+', r'\1', regex=True)
 
-#convert categories into arrays
-data["categories"] = data["categories"].str.split(' ') 
+#convert categories into arrays - don't do
+#data["categories"] = data["categories"].str.split(' ') 
 
 #TODO: Encode categories (run into ValueError: setting an array element with a sequence if not)
 le = preprocessing.LabelEncoder()
 le.fit(data['categories'])
 data['encoded_categories'] = le.transform(data['categories'])
-data.head()
 
 #TODO: tfidf
 #defaults at first
 #tweak pending results 
 vectorizer = TfidfVectorizer(max_df = 1.0) #edit max_df as we see fit
-abstracts = data.abstract.astype(str) #can't pass tokens through vectorizer 
-tfidf_matrix = vectorizer.fit_transform(abstracts)
+#abstracts = data.abstract.astype(str) #can't pass tokens through vectorizer 
+tfidf_matrix = vectorizer.fit_transform(data['lemma abstract'])
 features = vectorizer.get_feature_names_out()
 
 #TODO: normalize # of vectors within each row
+#QUESTION: do we need to do this if we over/under sample the # of articles between each categories? 
 
 #split into test and train data
-X = tfidf_matrix
+X = tfidf_matrix #QUESTION: How is the relationship between words and y-target captured? 
 y = data['encoded_categories']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
